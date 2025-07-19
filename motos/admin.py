8f9908095b09dd1e6e8173a11c744a3de6bba177
Marcelo Borges <marcelosbgs@gmail.com>
2025-07-18 22:01:19 -0300

    Projeto Django Motos JJT BD configurado e com fotos ok

---
+++ motos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Motos, Brand
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class MotosAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factor_year', 'model_year', 'value')
+    search_fields = ('model',)
+
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Motos, MotosAdmin)
