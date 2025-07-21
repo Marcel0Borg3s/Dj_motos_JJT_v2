f61274107c40df6e4110a1ff4e1559a61877d311
Marcelo Borges <marcelosbgs@gmail.com>
2025-07-21 06:50:48 -0300

    iniciando templates

--- app/urls.py
+++ app/urls.py
@@ -18,9 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from motos.views import motos_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('motos/', motos_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
