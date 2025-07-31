56de954899fcce45f74e1b9013bdb48d785a1f67
Marcelo Borges <marcelosbgs@gmail.com>
2025-07-31 05:41:29 -0300

    #58 finalizado o forms e validação de dados

--- motos/forms.py
+++ motos/forms.py
@@ -28,4 +28,15 @@ class MotoModelForm(forms.ModelForm):
         model = Motos
         fields = '__all__'       
     
- 
+    # Crianda validação/erros a partir de dados definidos
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value <= 999:
+            self.add_error('value', "O valor deve ser maior que 999.")
+        return value
+    
+    def clean_factor_year(self):
+        factor_year = self.cleaned_data.get('factor_year')
+        if factor_year < 1980:
+            self.add_error('factor_year', "O ano do fator deve ser maior ou igual a 1980.")
+        return factor_year
