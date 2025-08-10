f73d07f026a4b9ee09bd20b19e03b4c27269f152
Marcelo Borges <marcelosbgs@gmail.com>
2025-08-10 08:57:44 -0300

    #66 atualização de new_moto_vie para Based View

--- app/urls.py
+++ app/urls.py
@@ -1,32 +1,16 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from motos.views import motos_view, new_moto_view
+from motos.views import MotosView, NewMotoView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('motos/', motos_view, name='motos_list'),
+    path('motos/', MotosView.as_view(), name='motos_list'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('new_moto/', new_moto_view, name='new_moto'),
+    path('new_moto/', NewMotoView.as_view(), name='new_moto'),
     path('register/', register_view, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- motos/views.py
+++ motos/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render, redirect
 from motos.models import Motos
 from motos.forms import MotoModelForm
+from django.views import View
 
-def motos_view(request):
-    motos = Motos.objects.all().order_by('model')
-    search = request.GET.get('search')
+class MotosView(View):
+    def get(self, request):
+        motos = Motos.objects.all().order_by('model')
+        search = request.GET.get('search')
 
-    if search:
-        motos = motos.filter(model__contains=search)
+        if search:
+            motos = motos.filter(model__contains=search)
 
-    return render(
-        request, 
-        'motos.html', 
-        {'motos': motos }
+        return render(
+            request, 
+            'motos.html', 
+            {'motos': motos }
+        )
 
-    )
+class NewMotoView(View):
 
-def new_moto_view(request):
-    if request.method == 'POST':
+    def get(self, request):
+        new_moto_form = MotoModelForm()
+        return render(request, 'new_moto.html', { 'new_moto_form': new_moto_form})
+
+    def post(self, request):
         new_moto_form = MotoModelForm(request.POST, request.FILES)
         if new_moto_form.is_valid():
             new_moto_form.save()
             return redirect('motos_list')
-         
-    else:
-        new_moto_form = MotoModelForm()
-    return render(request, 'new_moto.html', { 'new_moto_form': new_moto_form})
+        return render(request, 'new_moto.html', { 'new_moto_form': new_moto_form})
+
 
